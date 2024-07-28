5a9b88b30ecd20b929dba1dde49e6a9378cf6e2e
MetalSnow <aymanetrend@gmail.com>
2024-07-28 10:46:58 +0800

    Implement path tracking in knightMoves function
    Added a parent map to track each position's parent

--- knight.js
+++ knight.js
@@ -18,34 +18,51 @@ function knightMoves(start, end) {
   ];
   let queue = [start];
   let visited = new Set();
-  let steps = 0;
+  let parentMap = new Map();
+
+  visited.add(start.toString());
 
   while (queue.length > 0) {
-    let next = [];
-    while (queue.length > 0) {
-      let current = queue.shift();
-      let currentX = current[0];
-      let currentY = current[1];
+    let current = queue.shift();
+    let currentX = current[0];
+    let currentY = current[1];
 
-      if (currentX === end[0] && currentY === current[1]) {
-        return steps;
+    if (currentX === end[0] && currentY === end[1]) {
+      let path = [];
+
+      let node = current;
+
+      while (node) {
+        path.push(node);
+        node = parentMap.get(node.toString());
       }
 
-      for (let d of possibleMoves) {
-        let nextX = currentX + d[0];
-        let nextY = currentY + d[1];
+      console.log(
+        `You made it in ${path.length - 1} moves!  Here's your path:`
+      );
 
-        if (isValid([nextX, nextY])) {
-          if (!visited.has(`${nextX},${nextY}`)) {
-            visited.add(`${nextX},${nextY}`);
-            next.push([nextX, nextY]);
-          }
+      path.reverse().forEach((pos) => {
+        console.log(pos);
+      });
+
+      return;
+    }
+
+    for (let d of possibleMoves) {
+      let nextX = currentX + d[0];
+      let nextY = currentY + d[1];
+      let nextPos = [nextX, nextY];
+
+      if (isValid(nextPos)) {
+        if (!visited.has(nextPos.toString())) {
+          visited.add(nextPos.toString());
+          parentMap.set(nextPos.toString(), current);
+          queue.push(nextPos);
         }
       }
     }
-    steps += 1;
-    queue = next;
   }
 }
 
-console.log(knightMoves([0, 0], [3, 3]));
+knightMoves([0, 0], [3, 3]); // You made it in 2 moves!  Here's your path:([ 0, 0 ],[ 1, 2 ],[ 3, 3 ])
+knightMoves([3, 3], [7, 7]); // You made it in 4 moves!  Here's your path:([ 3, 3 ],[ 1, 4 ],[ 3, 5 ],[ 5, 6 ],[ 7, 7 ])
